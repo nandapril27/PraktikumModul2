91532b663a74cb287b260dbee0a6cc274045af1c
nandapril27 <nandapril27@gmail.com>
2024-03-19 13:53:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikummodul2/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.praktikummodul2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import
+        android.widget.CheckBox;
+import
+        android.widget.EditText;
+import
+        android.widget.RadioButton;
+import
+        android.widget.RadioGroup;
+import
+        android.widget.TextView;
+
+public class MainActivity
+        extends AppCompatActivity {
+    int gajiStatus, gajiGolongan;
+    RadioGroup golongan;
+    CheckBox status;
+
+    @Override
+    public void onCreate(Bundle
+                                 savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void hitung(View v) {
+        EditText nama = (EditText) findViewById(R.id.nama);
+        TextView outputNama = (TextView) findViewById(R.id.outputNama);
+        String name = nama.getText().toString();
+        outputNama.setText("Nama: " + name);
+
+        status = (CheckBox) findViewById(R.id.menikah);
+        String statusText = status.isChecked() ? "Status: Menikah" : "Status: Belum Menikah";
+
+        golongan = (RadioGroup) findViewById(R.id.rgGolongan);
+        int gol = golongan.getCheckedRadioButtonId();
+        String golonganText = "";
+        if (gol == R.id.rbGolongan1) {
+            golonganText = "Golongan: 1";
+        } else if (gol == R.id.rbGolongan2) {
+            golonganText = "Golongan: 2";
+        }
+
+        gajiStatus = status.isChecked() ? 500000 : 0;
+        gajiGolongan = gol == R.id.rbGolongan1 ? 1000000 : (gol == R.id.rbGolongan2 ? 2000000 : 0);
+
+        int totalGaji = gajiStatus + gajiGolongan;
+        TextView total = (TextView) findViewById(R.id.outputGaji);
+        total.setText("Total Gaji Anda: " + totalGaji);
+
+        TextView statusView = (TextView) findViewById(R.id.outputStatus);
+        statusView.setText(statusText);
+
+        TextView golonganView = (TextView) findViewById(R.id.outputGolongan);
+        golonganView.setText(golonganText);
+    }
+
+}
